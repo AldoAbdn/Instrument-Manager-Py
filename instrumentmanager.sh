7fd1dff5c417466f8cac46c3b920147ff4d9f447
Alistair Quinn <alistairaldo@hotmail.co.uk>
2021-02-08 14:02:37 +0000

    Added a shell script to launch kiosk

---
+++ instrumentmanager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 "/home/pi/Documents/Instrument-Manager-Py/InstrumentManager.py" -s & sleep 10; /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://0.0.0.0:5000
+
+
